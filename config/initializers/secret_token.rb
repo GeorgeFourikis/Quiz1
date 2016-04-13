395f3b6da92ae8e522817800096f8173bdc483b7
GeorgeFourikis <you@example.cogit config --global user.email you@example.co>
2016-04-13 09:32:15 +0000

    Init Quiz1 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiz1::Application.config.secret_key_base = 'a076592a6b77bcc3201da20378a04655e2a3bcdee5b0352a8d534a5a4f98abe332eb1605a058e77c5288fe89cedd86eda01a6c0abe0ef4c71809168c54c52da3'
